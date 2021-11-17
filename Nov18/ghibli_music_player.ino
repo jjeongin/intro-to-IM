56d42bdffe315f2a3e5da50c334e81c58d17caab
Jeongin Lee <68997923+jjeongin@users.noreply.github.com>
2021-11-17 18:30:34 +0400

    Add files via upload

--- Nov18/ghibli_music_player.ino
+++ Nov18/ghibli_music_player.ino
@@ -5,13 +5,11 @@ const int switchPin = 13; // digital input (switch)
 const int potPin = A2; // analog input (potentiometer)
 
 // Music 1: TOTORO
-// melody array referring to <pitch.h>
-int totoro_melody[] = {
+int totoro_melody[] = { // melody array referring to <pitch.h>
   NOTE_D5, NOTE_B4, NOTE_G4, NOTE_D5, NOTE_C5, NOTE_A4, 0, 0, 0
 };
 
-// note durations: 4 = quarter note, 8 = eighth note, etc.
-int totoro_durations[] = {
+int totoro_durations[] = { // note durations: 4 = quarter note, 8 = eighth note, etc.
   2, 4, 2, 2, 2, 1, 4, 4, 4
 };
 
@@ -23,7 +21,7 @@ int spirited_away_melody[] = {
 
 int spirited_away_durations[] = {
   4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 4, 4, 4, 4,
-  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 0, 0
+  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 4, 4
 };
 
 
@@ -34,19 +32,17 @@ void setup() {
 
 void loop() {
   int angle = analogRead(potPin); // read angle from potentiometer
-//  angle = map(angle, 0, 1023, 0, 180);
-  Serial.println(angle);
-  delay(1);
+  angle = map(angle, 0, 1023, 0, 270); // since potentiometer angle can rotate from 0 to 270 degree
   
   if (digitalRead(switchPin) == HIGH) { // if the switch is pushed
-    if (angle <= 90) { // if potentiometer angle is smaller than 90, play totoro
+    if (angle <= 135) { // if potentiometer angle is smaller than 135, play totoro
       for (int i = 0; i < sizeof(totoro_melody) / sizeof(totoro_melody[0]); i++) {
         int duration = 1000 / totoro_durations[i]; // calculate the note duration, 1000 millisec = 1 sec (reference: Melody example in Arduino)
         tone(spkrPin, totoro_melody[i], duration); // play each note in the melody
         delay(duration * 1.30); // delay whole program to make sure each note has enough time before playing the next note (reference: Melody example in Arduino)
       }
     }
-    else { // if potentiometer angle is bigger than 90, play spirited away
+    else { // if potentiometer angle is bigger than 135, play spirited away
       for (int i = 0; i < sizeof(spirited_away_melody) / sizeof(spirited_away_melody[0]); i++) {
         int duration = 1000 / spirited_away_durations[i]; // calculate the note duration, 1000 millisec = 1 sec (reference: Melody example in Arduino)
         tone(spkrPin, spirited_away_melody[i], duration); // play each note in the melody
@@ -54,6 +50,4 @@ void loop() {
       }
     }
   }
-
-
 }
